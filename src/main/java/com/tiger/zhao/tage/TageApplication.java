50ce7849a28c998f81fd2f8f8d0d089a1e3e6e63
unknown <117676896@qq.com>
2018-11-06 16:34:26 +0800

    test

---
+++ src/main/java/com/tiger/zhao/tage/TageApplication.java
@@ -0,0 +1,12 @@
+package com.tiger.zhao.tage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TageApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tiger/zhao/tage/controller/HelloDemoController.java
@@ -0,0 +1,17 @@
+package com.tiger.zhao.tage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloDemoController {
+    @RequestMapping("/hello")
+    public Map sayHello(){
+        Map result = new HashMap<>();
+        result.put("name","tiger");
+        return result;
+    }
+}
